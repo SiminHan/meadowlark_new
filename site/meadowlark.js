0ad4d569b1c4f08d3d4b8ad4415587216a8c078a
SiminHan <407881938@qq.com>
2016-04-30 18:00:13 +0800

    add view and handlebars
    ch3.3.1

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -3,31 +3,38 @@
  */
 var express=require('express');
 var app=express();
+var handlebars=require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
 app.set('port',process.env.PORT||3000);
 
 app.get('/',function(req,res){
-   res.type('text/plain');
-    res.send('Meadowlark Travel');
+  // res.type('text/plain');
+    //res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about',function(req,res){
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+   // res.type('text/plain');
+   // res.send('About Meadowlark Travel');
+    res.render('about');
 });
 
 //404
 app.use(function(req,res){
-    res.type('text/plain');
+   // res.type('text/plain');
     res.status(404);
-    res.send('404-Not found');
+   // res.send('404-Not found');
+    res.render('404');
 });
 
 //500
 app.use(function(err,req,res,next){
     console.error(err.stack);
-    res.type('text/plain');
+   // res.type('text/plain');
     res.status(500);
-    res.send('500-Server Error');
+    //res.send('500-Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'),function(){
